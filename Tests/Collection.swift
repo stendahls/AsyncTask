8da45ca2e9ac4d06f48e9cf1c4783840c339b1ad
Thomas Sempf <thomas.sempf@stendahls.se>
2017-03-16 09:25:00 +0100

    Added new unit test for collections.

--- Tests/Collection.swift
+++ Tests/Collection.swift
@@ -1,120 +1,157 @@
-//import AsyncTask
 //
+//  Collection.swift
 //
-//class CollectionSpec: QuickSpec {
-//    override func spec() {
-//        describe("array of tasks") {
-//            it("should run serially inside for loops") {
-//                var results = [String]()
-//                for number in numbers {
-//                    results.append(toString(number).await())
-//                }
-//                expect(results) == numbers.map {number in "\(number)"}
-//            }
+//  Created by Thomas Sempf on 2017-01-10.
+//  Copyright © 2017 Stendahls. All rights reserved.
 //
-//            it("should run serially inside map") {
-//                let results = numbers.map {number in toString(number).await()}
-//                expect(results) == numbers.map {number in "\(number)"}
-//            }
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//            it("should in parallel") {
-//                for _ in 0..<1000 {
-//                    let results = numbers.map(toString).awaitAll()
-//                    expect(results).to(contain("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"))
-//                }
-//            }
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
-//            it("should in parallel") {
-//                for _ in 0..<10 {
-//                    let results = (0..<500).map({n in toStringAfter(n, timeoutInterval: 0.0001)}).awaitAll()
-//                    expect(results.count) == 500
-//                }
-//            }
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
-//            it("should in parallel") {
-//                let results = (0..<5000).map({n in toStringAfter(n, timeoutInterval: 0.0001)}).awaitAll()
-//                expect(results.count) == 5000
-//            }
-//        }
-//
-//        describe("dictionary of tasks") {
-//            it("should run a dictionary of closures in parallel") {
-//                var tasks = [Int: Task<String>]()
-//                for (index, element) in numbers.map(toString).enumerate() {
-//                    let key = numbers[index]
-//                    tasks[key] = element
-//                }
-//                let results = tasks.await()
-//
-//                var expected = [Int: String]()
-//                for number in numbers {
-//                    expected[number] = "\(number)"
-//                }
-//                expect(results.count) == expected.count
-//                for (key, _) in expected {
-//                    expect(expected[key]) == results[key]
-//                }
-//            }
-//        }
-//
-//        describe("array of tasks") {
-//            it("can await first") {
-//                let result = numbers.shuffle().map {number in toStringAfter(number, timeoutInterval: NSTimeInterval(number + 1))}.awaitFirst()
-//                expect(result) == "0"
-//            }
-//
-//            it("can await first") {
-//                let task1 = Task<String?> {
-//                    timeout(2).await()
-//                    return "aa"
-//                }
-//
-//                let task2 = Task<String?> {
-//                    timeout(1).await()
-//                    return nil
-//                }
-//
-//                expect{[task1, task2].awaitFirst()}.to(beNil())
-//            }
-//        }
-//
-//        describe("array of throwing tasks") {
-//            it("should throw if any task throws") {
-//                expect{try numbers.map(toStringExceptZero).awaitAll()}.to(throwError())
-//            }
-//        }
-//    }
-//}
-//
-//
-//let numbers: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//
-//func toString(number: Int) -> Task<String> {
-//    return Task { "\(number)" }
-//}
-//
-//func timeout(timeout: NSTimeInterval) -> Task<Void> {
-//    return Task { NSThread.sleepForTimeInterval(timeout) }
-//}
-//
-//func toStringAfter(number: Int, timeoutInterval: NSTimeInterval) -> Task<String> {
-//    return Task {
-//        timeout(timeoutInterval).await()
-//        return toString(number).await()
-//    }
-//}
-//
-//enum Error : ErrorType {
-//    case FoundZero
-//}
-//
-//let toStringExceptZero = {(number: Int) -> ThrowableTask<String> in
-//    ThrowableTask {
-//        if number == 0 {
-//            throw Error.FoundZero
-//        }
-//        return "\(number)"
-//    }
-//}
-//
-//
+
+import XCTest
+@testable import AsyncTask
+
+let numbers: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+func toString(_ number: Int) -> Task<String> {
+    return Task { "\(number)" }
+}
+
+func timeout(forTimeInterval timeout: TimeInterval) -> Task<Void> {
+    return Task { Thread.sleep(forTimeInterval: timeout) }
+}
+
+func toStringAfter(_ number: Int, timeoutInterval: TimeInterval) -> Task<String> {
+    return Task {
+        timeout(forTimeInterval: timeoutInterval).await()
+        return toString(number).await()
+    }
+}
+
+enum TestError : Error {
+    case FoundZero
+}
+
+let toStringExceptZero = {(number: Int) -> ThrowableTask<String> in
+    ThrowableTask {
+        if number == 0 {
+            throw TestError.FoundZero
+        }
+        return "\(number)"
+    }
+}
+
+class CollectionTaskTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testThatAwaitWorksInsideMap() {
+        let results = numbers.map {number in toString(number).await()}
+        XCTAssert(results == numbers.map {number in "\(number)"})
+    }
+    
+    func testThatAwaitAllWorksWithArrayOfTasks() {
+        for _ in 0..<1000 {
+            let results = numbers.map(toString).awaitAll()
+            
+            numbers.forEach({ number in
+                XCTAssert(results.contains("\(number)"))
+            })
+        }
+    }
+    
+    func testThatAwaitAllWorksWithArrayOfNestedTasks() {
+        for _ in 0..<10 {
+            let results = (0..<500).map({n in toStringAfter(n, timeoutInterval: 0.0001)}).awaitAll()
+            XCTAssert(results.count == 500)
+        }
+        
+        let results = (0..<5000).map({n in toStringAfter(n, timeoutInterval: 0.0001)}).awaitAll()
+        XCTAssert(results.count == 5000)
+    }
+
+    func testThatTasksInDictionarOfClosuresRunInParallel() {
+        var tasks = [Int: Task<String>]()
+        
+        for (index, element) in numbers.map(toString).enumerated() {
+            let key = numbers[index]
+            tasks[key] = element
+        }
+        let results = tasks.await()
+        
+        var expected = [Int: String]()
+        for number in numbers {
+            expected[number] = "\(number)"
+        }
+        
+        XCTAssert(results.count == expected.count)
+        
+        for (key, _) in expected {
+            XCTAssert(expected[key] == results[key])
+        }
+    }
+    
+    func testThatAwaitFirstWorks() {
+        let result = numbers
+            .shuffle()
+            .map {number in toStringAfter(number, timeoutInterval: TimeInterval(number + 1))}
+            .awaitFirst()
+        
+        XCTAssert(result == "0")
+    }
+    
+    func testThatAwaitFirstWorksWithOptionals() {
+        let task1 = Task<String?> {
+            timeout(forTimeInterval: 2).await()
+            return "aa"
+        }
+        
+        let task2 = Task<String?> {
+            timeout(forTimeInterval: 1).await()
+            return nil
+        }
+        
+        XCTAssert([task1, task2].awaitFirst() == nil)
+    }
+    
+    func testThatAwaitAllWorksWithThrowableTasks() {
+        XCTAssertThrowsError(try numbers.map(toStringExceptZero).awaitAll())
+    }
+    
+    func testThatAwaitFirstWorksWithThrowableTasks() {
+        let task1 = ThrowableTask<String> {
+            timeout(forTimeInterval: 0.1).await()
+            let result = try toStringExceptZero(0).await()
+            return result
+        }
+        
+        let task2 = ThrowableTask<String> { () -> String in
+            timeout(forTimeInterval: 1).await()
+            return "aa"
+        }
+        
+        XCTAssertThrowsError(try [task1, task2].awaitFirst())
+    }
+}
